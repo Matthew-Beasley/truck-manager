ad2bf5f43d446bff2eda15289f99af9b7a95fa65
conbec <56614504+conbec@users.noreply.github.com>
2020-02-29 18:25:59 -0800

    datalayer and api file skeleton in place

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router, //or HashRouter
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+const App = () => {
+  return (
+    <h1>Hello from react!</h1>
+  )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  BrowserRouter as Router, //or HashRouter
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
-const App = () => {
-  return (
-    <h1>Hello from react!</h1>
-  )
-}
+import App from './App';
 
 const root = document.querySelector('#root');
 ReactDOM.render(<App />, root);
