cb0e971021ab695b0a63761a9f5c3a18dcc8395f
conbec <56614504+conbec@users.noreply.github.com>
2020-02-29 17:06:36 -0800

    webpack build working, server working, react working

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
+
+
+app.get('/', (req, res, next) => {
+  console.log(req.body)
+  try {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  } catch (error) {
+    next(error);
+  }
+});
+
+//maybe rethink this error handling
+app.use((req, res, next) => {
+  next({
+    status: 404,
+    message: `Page not found for ${req.method} ${req.url}`
+  })
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).send({
+    message: err.message || JSON.stringify(err)
+  });
+});
+
+
+app.listen(PORT, () => console.log('Listening on PORT ', PORT));
